209955cbc5fde76ca6a4805bd6f9e62f144fdcc9
Oscar Anillo <oscarani14@gmail.com>
2023-09-06 18:15:56 -0500

    clean from uncessary files

---
+++ FRONTEND/front-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>Oscar Anillo</h1>
+      <p>Fullstack Developer</p>
+    </div>
+  )
+}
+
+export default App
